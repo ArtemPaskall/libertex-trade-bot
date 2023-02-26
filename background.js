589bd69fd0bda267da1044cc84bf0451e1ef3ccb
ArtemPaskall <artempaskal@gmail.com>
2023-02-26 12:38:26 +0200

    sequential script execution  bug fixing

--- background.js
+++ background.js
@@ -1,11 +1,11 @@
 chrome.tabs.onUpdated.addListener((tabId, info, tab) => {
-      if (tab.url && tab.url.includes("https://tilda.cc/page/")) {
-      const queryParameters = tab.url.split("?")[1];
-      const urlParameters = new URLSearchParams(queryParameters);
+  if (tab.url && tab.url.includes('https://tilda.cc/page/')) {
+    const queryParameters = tab.url.split('?')[1]
+    const urlParameters = new URLSearchParams(queryParameters)
 
-      chrome.tabs.sendMessage(tabId, {
-        type: "NEW",
-        pageId: urlParameters.get("pageid"),
-      });
-    } 
-  });
+    chrome.tabs.sendMessage(tabId, {
+      type: 'NEW',
+      pageId: urlParameters.get('pageid'),
+    })
+  }
+})
--- my-script.js
+++ my-script.js
@@ -1,57 +1,61 @@
 console.log('Injecting script...')
 
-// setTimeout(() => {
-//   console.log('[INJECTION]: window.ace', window.ace)
-//   console.log('[INJECTION]: window.ace.edit', window.ace.edit)
-// }, 3000)
-
 let editor = undefined
 
 document.addEventListener('patch-ace', () => {
-  console.log('[INJECTION]: patch-ace event catched, going to patch ace.edit...')
-  console.log('[INJECTION:PATCH-ACE]: at once: window.ace', window.ace)
-  setTimeout(() => {
-    console.log('[INJECTION:PATCH-ACE]: later: window.ace', window.ace)
-    console.log('[INJECTION:PATCH-ACE]: later: window.ace.edit', window.ace.edit)
 
+
+
+  setTimeout( async () => {
     originalEdit = window.ace.edit
 
     window.ace.edit = name => {
       editor = originalEdit(name)
-      // if (!window.aceEditors) {
-      //   window.aceEditors = {}
-      // }
-      // window.aceEditors[name] = editor
-      console.log('[INJECTION:PATCH-ACE:EDIT]: tilda created editor:', editor)
       document.dispatchEvent(new Event('ace-editor-created'))
+
       return editor
     }
+
+
+
+    const saveButton = await waitForElement('.tbtn.tbtn-primary')
+    saveButton.click()
+
+    console.log('patch-ace finished');
   }, 2000)
 })
 
+
+
+
+
 document.addEventListener('set-value-to-editor', e => {
-  console.log('going to set value to editor')
   setTimeout(() => {
     editor.setValue(e.detail.value)
   }, 1000)
 })
 
-// function waitForElement(selector) {
-//   return new Promise(resolve => {
-//       if (document.querySelector(selector)) {
-//           return resolve(document.querySelector(selector));
-//       }
 
-//       const observer = new MutationObserver(mutations => {
-//           if (document.querySelector(selector)) {
-//               resolve(document.querySelector(selector));
-//               observer.disconnect();
-//           }
-//       });
 
-//       observer.observe(document.body, {
-//           childList: true,
-//           subtree: true
-//       });
-//   });
-// }
+
+
+
+function waitForElement(selector) {
+  return new Promise(resolve => {
+    if (document.querySelector(selector)) {
+      return resolve(document.querySelector(selector))
+    }
+
+    const observer = new MutationObserver(mutations => {
+      if (document.querySelector(selector)) {
+        resolve(document.querySelector(selector))
+        observer.disconnect()
+      }
+    })
+
+    observer.observe(document.body, {
+      childList: true,
+      subtree: true,
+    })
+  })
+}
