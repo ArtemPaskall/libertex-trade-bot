4bed4c9d58c17f9ca211679a45b4fd1326879bca
ArtemPaskall <artempaskal@gmail.com>
2023-03-05 14:55:01 +0200

    Demo version commit

--- contentScript.js
+++ contentScript.js
@@ -12,7 +12,6 @@
   }
   injectScript(chrome.runtime.getURL('/my-script.js'), 'body')
 
-
   chrome.runtime.onMessage.addListener((obj, sender, response) => {
     const { type } = obj
 
@@ -21,13 +20,7 @@
     }
   })
 
-
-
-
-
   const newPageLoaded = () => {
-    console.log('newPageLoaded')
-
     const HTMLPublishBtnExist = document.querySelector('#HTMLPublishBtn')
 
     if (!HTMLPublishBtnExist) {
@@ -48,15 +41,7 @@
     }
   }
 
-
-
-
-
-
-
-
   const HTMLPublishHandler = () => {
-    console.log('HTML Publish')
     const mainContent = document.querySelector('#allrecords')
 
     const elementsToPublish = [...mainContent.children].filter(
@@ -85,166 +70,102 @@
       return elementsWithPath.length > 0 ? true : false
     }
 
-
-
-    const createLoader = (loaderId) => {
-       const darkWrapOnScriptExecuting = document.createElement('div')
+    const createLoader = loaderId => {
+      const darkWrapOnScriptExecuting = document.createElement('div')
       darkWrapOnScriptExecuting.id = loaderId
       darkWrapOnScriptExecuting.innerText = 'Loading...'
-      darkWrapOnScriptExecuting.style = 'position: fixed; top: 0; left: 0;' +
-      'width: 100%; height: 100%; background: rgba(0, 0, 0, 0.5); z-index: 1000000; ' +
-      'color: #fff; font-size: 52px;' +
-      'display: flex; justify-content: center; align-items: center;'
-      document.body.appendChild(darkWrapOnScriptExecuting);
+      darkWrapOnScriptExecuting.style =
+        'position: fixed; top: 0; left: 0;' +
+        'width: 100%; height: 100%; background: rgba(0, 0, 0, 0.5); z-index: 1000000; ' +
+        'color: #fff; font-size: 52px;' +
+        'display: flex; justify-content: center; align-items: center;'
+      document.body.appendChild(darkWrapOnScriptExecuting)
     }
 
-    const removeLoader = (loaderId) => {
+    const removeLoader = loaderId => {
       const loaderToRemove = document.getElementById(loaderId)
       loaderToRemove.remove()
     }
 
-
-
-    // elementsToPublishWithPath.forEach(element => HTMLPublish(element))
-    // HTMLPublish(elementsToPublishWithPath[0])
-
-    const elementsPublishPromise = elementsToPublishWithPath.reduce((promise, element, index) => {
-      return promise.then(() => {
-        return new Promise(resolve => {
-
-
-          console.log(`Processing item ${index}`);
-          let patched = false
-          HTMLPublish(element, patched)
-
-          setTimeout(() => {
-            console.log('second execution of HTMLPublish started');
-            let patched = true
+    const elementsPublishPromise = elementsToPublishWithPath.reduce(
+      (promise, element, index) => {
+        return promise.then(() => {
+          return new Promise(resolve => {
+            let patched = false
             HTMLPublish(element, patched)
-            console.log('second execution of HTMLPublish finished');
-          }, 7500);
 
+            setTimeout(() => {
+              let patched = true
+              HTMLPublish(element, patched)
+            }, 7500)
 
-
-          setTimeout(() => {
-            console.log(`Finished processing item ${index}`);
-            resolve();
-            console.log(`5s passed`);
-
-          }, 16000);
-        });
-      });
-    }, Promise.resolve(createLoader('wrapperDark')));
-
+            setTimeout(() => {
+              resolve()
+            }, 16000)
+          })
+        })
+      },
+      Promise.resolve(createLoader('wrapperDark')),
+    )
 
     elementsPublishPromise.then(() => {
       removeLoader('wrapperDark')
     })
   }
 
-
-  // let patched = false
-
   async function HTMLPublish(blockToPublish, patched) {
-    console.log('patched', patched);
-
     if (!patched) {
-    const mainContent = document.querySelector('#allrecords')
-    const blocksToPublishIDArray = [...mainContent.children].map(
-      child => child.id,
-    )
+      const mainContent = document.querySelector('#allrecords')
 
-    const indexOfBlockToPublish = blocksToPublishIDArray.indexOf(
-      blockToPublish.id,
-    )
+      const findOnOffButton = element => {
+        const onOffButton = [...element.querySelectorAll('*')].filter(
+          child => child.title === 'Спрятать/Показать',
+        )
 
-    const findOnOffButton = (element) => {
-      const onOffButton = [...element.querySelectorAll('*')].filter(
+        return onOffButton
+      }
+
+      const findMooveUpButton = element => {
+        const mooveUpButton = [...element.querySelectorAll('*')].filter(
+          child => child.title === 'Переместить вверх',
+        )
+
+        return mooveUpButton
+      }
+
+      const onOffButton = [...blockToPublish.querySelectorAll('*')].filter(
         child => child.title === 'Спрятать/Показать',
       )
 
-      return onOffButton
-    }
-
-    const findMooveUpButton = (element) => {
-      const mooveUpButton = [...element.querySelectorAll('*')].filter(
-        child => child.title === 'Переместить вверх',
+      const contentEditBtn = [...blockToPublish.querySelectorAll('*')].filter(
+        child =>
+          child.innerText === 'Контент' &&
+          child.className === 'recordedit_mainleft_but_settings_title',
       )
 
-      console.log('mooveUpButton', mooveUpButton);
-      return mooveUpButton
-    }
+      const createCopyBlockToPublishBtn = [
+        ...blockToPublish.querySelectorAll('*'),
+      ].filter(child => child.title === 'Дублировать')
 
-
-    const onOffButton = [...blockToPublish.querySelectorAll('*')].filter(
-      child => child.title === 'Спрятать/Показать',
-    )
-
-
-
-    const contentEditBtn = [...blockToPublish.querySelectorAll('*')].filter(
-      child =>
-        child.innerText === 'Контент' &&
-        child.className === 'recordedit_mainleft_but_settings_title',
-    )
-
-    const createCopyBlockToPublishBtn = [
-      ...blockToPublish.querySelectorAll('*'),
-    ].filter(child => child.title === 'Дублировать')
-
-    createCopyBlockToPublishBtn[0].click()
-
-
-    setTimeout(() => {
-      const sibling = blockToPublish.nextElementSibling
-      console.log('blockToPublish.nextSibling', sibling);
-      findOnOffButton(sibling)[0].click()
-      // findMooveUpButton(sibling)[0].click()
-      console.log('2500 left');
+      createCopyBlockToPublishBtn[0].click()
 
       setTimeout(() => {
-        // const sibling = blockToPublish.nextElementSibling
-        // console.log('blockToPublish.nextSibling', sibling);
-        // findOnOffButton(sibling)[0].click()
-        findMooveUpButton(sibling)[0].click()
-        console.log('+1500 left');
-        }, 1000);
+        const sibling = blockToPublish.nextElementSibling
+        findOnOffButton(sibling)[0].click()
 
+        setTimeout(() => {
+          findMooveUpButton(sibling)[0].click()
+        }, 1000)
+      }, 2500)
 
-   }, 2500);
+      setTimeout(() => {
+        contentEditBtn[0].click()
+      }, 4000)
 
-
-    // console.log('blockToPublish', blockToPublish);
-    // const sibling = blockToPublish.nextElementSibling
-    // console.log('blockToPublish.nextSibling', sibling);
-    // console.log('blockToPublish.nextSibling.className', blockToPublish.nextSibling.id);
-    // contentEditBtn[0].click()
-
-    setTimeout(() => {
-      contentEditBtn[0].click()
-      console.log('contentEditBtn[0].click()');
-   }, 4000);
-
-
-   setTimeout( async () => {
-
-    document.dispatchEvent(
-      new CustomEvent('patch-ace'))
-      console.log('patch-ace in contentScript')
-
-
-    // const saveButton = await waitForElement('.tbtn.tbtn-primary')
-    // saveButton.click()
-    // console.log(saveButton);
-
-   }, 5000);
-
-
-
-
+      setTimeout(async () => {
+        document.dispatchEvent(new CustomEvent('patch-ace'))
+      }, 5000)
     } else {
-      console.log('patched', patched);
-
       let absolutePath = [...blockToPublish.querySelectorAll('*')].filter(
         element => element.className === 'hljs-value',
       )[0].innerText
@@ -253,6 +174,7 @@
       const endIndex = absolutePath.indexOf('md') + 2
       absolutePath = absolutePath.slice(startIndex, endIndex)
       absolutePath = encodeURIComponent(absolutePath)
+
       async function MDContentFetch() {
         try {
           const response = await fetch(
@@ -272,15 +194,6 @@
       }
 
       const MDContent = await MDContentFetch()
-      // console.log(MDContent);
-
-
-
-
-
-      document.addEventListener('ace-editor-created', () => {
-        console.log('ace-editor-created executed in contentScript');
-      })
 
       const MDContentScriptTagWrapped =
         '<zero-md>\n' +
@@ -289,14 +202,13 @@
         '</script>\n' +
         '</zero-md>'
 
-        const contentEditBtn = [...blockToPublish.querySelectorAll('*')].filter(
-          child =>
-            child.innerText === 'Контент' &&
-            child.className === 'recordedit_mainleft_but_settings_title',
-        )
+      const contentEditBtn = [...blockToPublish.querySelectorAll('*')].filter(
+        child =>
+          child.innerText === 'Контент' &&
+          child.className === 'recordedit_mainleft_but_settings_title',
+      )
 
-        contentEditBtn[0].click()
-
+      contentEditBtn[0].click()
 
       document.dispatchEvent(
         new CustomEvent('set-value-to-editor', {
@@ -305,26 +217,8 @@
           },
         }),
       )
-
-      console.log('set-value-to-editor in contentScript');
     }
-
-
-
-    console.log('HTMLPublish finish');
-    // console.log('finish render of element', blockToPublish);
-    // const src = chrome.runtime.getURL("src/ace.js")
-    // await import(src)
-    // const editor = ace.edit("aceeditor553653040")
-
-    // createCopyBlockToPublishBtn[0].click()
-    // onOffButton[0].click()
   }
-
-
-
-
-
 
   function waitForElement(selector) {
     return new Promise(resolve => {
@@ -345,6 +239,4 @@
       })
     })
   }
-
-
 })()
--- my-script.js
+++ my-script.js
@@ -1,14 +1,7 @@
-console.log('Injecting script...')
-
 let editor = undefined
 
-
-
-
 document.addEventListener('patch-ace', async () => {
-  console.log('patch-ace started');
-
- setTimeout(() => {
+  setTimeout(() => {
     let originalEdit = window.ace.edit
 
     window.ace.edit = name => {
@@ -17,99 +10,22 @@ document.addEventListener('patch-ace', async () => {
 
       return editor
     }
-
-    // console.log('ace-editor-created in my-script.js');
-    // console.log('patch-ace finished');
-    // console.log('4500 left');
   }, 500)
 
-
-
-  setTimeout( async () => {
-    console.log(editor);
+  setTimeout(async () => {
     const saveButton = await waitForElement('.tbtn.tbtn-primary')
     saveButton.click()
-    console.log('save button clicked');
-    console.log('5000 left');
-    // const HTMLButton = await waitForElement('#HTMLPublishBtn')
-    // HTMLButton.click()
-    // console.log('html button clicked');
   }, 1000)
-
-
-
-
-
-  let count = 0;
-
-  // const intervalId = setInterval(async () => {
-
-  //   if (window.ace?.edit) {
-  //     let originalEdit = window.ace.edit
-
-  //     window.ace.edit = name => {
-  //       editor = originalEdit(name)
-  //       document.dispatchEvent(new Event('ace-editor-created'))
-
-  //       return editor
-  //     }
-
-  //     console.log(count);
-  //     console.log('ace-editor-created in my-script.js');
-  //     console.log('patch-ace finished');
-  //     console.log('2sec left');
-
-  //     count++
-  //     clearInterval(intervalId)
-
-  //     const saveButton = await waitForElement('.tbtn.tbtn-primary')
-  //     saveButton.click()
-  //   }
-  // }, 500)
-
-
-
-
-
-  //   setTimeout( async () => {
-  //   console.log(editor);
-  //   const saveButton = await waitForElement('.tbtn.tbtn-primary')
-  //   saveButton.click()
-  //   console.log('save button clicked');
-  //   // const HTMLButton = await waitForElement('#HTMLPublishBtn')
-  //   // HTMLButton.click()
-  //   // console.log('html button clicked');
-
-  //   const qqq = await waitForElement('#HTMLPublishBtn2')
-  //   qqq.click()
-  //   console.log('html button clicked');
-
-
-  //   console.log('2500 sec left');
-  // }, 2500)
-
 })
 
-
-
 document.addEventListener('set-value-to-editor', e => {
-  setTimeout( async () => {
-    console.log(e);
-    console.log(editor);
+  setTimeout(async () => {
     editor.setValue(e.detail.value)
-
     const saveButton = await waitForElement('.tbtn.tbtn-primary')
     saveButton.click()
     originalEdit = undefined
-
   }, 1000)
-
 })
-
-
-
-
-
 
 function waitForElement(selector) {
   return new Promise(resolve => {
